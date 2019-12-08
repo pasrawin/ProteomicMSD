51c55ccd4e5c4d09fc64d1ad10a570e0e66ba1c0
pasrawin <54527915+pasrawin@users.noreply.github.com>
2019-12-08 23:08:54 +0900

    Optimize code optimization

--- mNMF08_HConstruction.py
+++ mNMF08_HConstruction.py
@@ -1,16 +1,19 @@
 from __future__ import division
 import numpy as np
+import math
 from scipy import sparse
 from scipy import spatial
 from scipy.stats import norm
+import timeit
+import gc
 
 ### FUNCTIONS ###
 from mNMF01_Tools import time_index, mz_index, smoothingtime_mat
 flatten = lambda fl: [item for sublist in fl for item in sublist]
 
 def h_construction(outfile, WithRTPrediction_, WithCosineH_, initRT_tuple, initRT_width,\
-					V_mat, W_mat, noise_number, globalparam_list, iso_maxnumber, gaussian_width, cos_cutoff):
-	
+					V_mat, W_mat, noise_number, globalparam_list, iso_maxnumber, gaussian_width, cos_cutoff, output_file):
+	gc.disable()
 	V_shape, W_shape = V_mat.shape, W_mat.shape
 	Hpeak_mean = 1*W_shape[0]/W_shape[1] #proofed per row
 	if WithRTPrediction_:
@@ -19,27 +22,57 @@ def h_construction(outfile, WithRTPrediction_, WithCosineH_, initRT_tuple, initR
 			exit()
 		H_mat, initRT_correct = h_prediction(initRT_tuple, initRT_width, \
 											Hpeak_mean, noise_number, globalparam_list, gaussian_width)
-		H_mat = H_mat.todense()
+		# H_mat = H_mat.todense()
 	else:
 		H_mat = h_constant(V_shape, W_shape, Hpeak_mean, noise_number)
 		initRT_correct = None
 
 	if WithCosineH_:
-		H_mat, H_cosmat, W_nonzerolist = h_cos(V_mat, W_mat, H_mat, iso_maxnumber, cos_cutoff)
+		H_size_rowcut = 250
+		V_mat = V_mat.tocsr()
+		W_mat = W_mat.tocsc()
+		H_mat = H_mat.tocsr()
+		
+		if H_mat.shape[0] > H_size_rowcut:
+			H_mat_list, H_cosmat_list = [], []			
+			for row in np.arange(0,H_mat.shape[0],H_size_rowcut):				
+				if row + H_size_rowcut >= H_mat.shape[0]:
+					row_end = H_mat.shape[0]
+				else:
+					row_end = row + H_size_rowcut
+				H_mat_out, H_cosmatdub_out = h_cos(V_mat, W_mat[:,row:row_end], H_mat[row:row_end,:], iso_maxnumber, cos_cutoff)
+				H_mat_list.append(H_mat_out)
+				del(H_mat_out)
+				H_cosmat_list.append(H_cosmatdub_out)
+				del(H_cosmatdub_out)
+
+			H_cosmat = sparse.vstack(H_cosmat_list)
+			H_cosmat = H_cosmat.tocoo()
+			del(H_cosmat_list)		
+			H_mat = sparse.vstack(H_mat_list)
+			del(H_mat_list)
+		else:
+			H_mat, H_cosmat = h_cos(V_mat, W_mat, H_mat, iso_maxnumber, cos_cutoff)	
 	else:
 		H_cosmat = None
 
 	if noise_number > 0:
 		noise_mat = np.zeros((noise_number,H_mat.shape[1]))
 		noise_mat += Hpeak_mean/H_mat.shape[1]
-		H_mat = np.vstack([H_mat, noise_mat])
-		# change to sparse if better for memory
-		if np.count_nonzero(H_mat)*3 < H_mat.size:
-			H_mat = sparse.coo_matrix(H_mat)
+		try:
+			H_mat = sparse.vstack([H_mat, noise_mat])
+			H_mat.tocoo()
+		except:
+			H_mat = np.vstack([H_mat, noise_mat])
+			# change to sparse if better for memory
+			if np.count_nonzero(H_mat)*3 < H_mat.size:
+				H_mat = sparse.coo_matrix(H_mat)
+	sparse.save_npz(str(output_file)+'_Hmat_init.npz', H_mat)
 	return H_mat, initRT_correct, H_cosmat
 
 ### subfunctions
 def h_prediction(initRT_tuple, initRT_width, Hpeak_mean, noise_number, globalparam_list, gaussian_width):
+	gc.disable()
 	for param in globalparam_list:
 		mm, mrange_min, mrange_max, mz_range, MZ_SCALE, \
         tt, gradient_starttime, gradient_endtime, gradient_time, TIME_SCALE, \
@@ -90,60 +123,65 @@ def h_constant(V_shape, W_shape, Hpeak_mean, noise_number):
 	return H_mat
 
 def h_cos(V_mat, W_mat, H_mat, iso_maxnumber, cos_cutoff):
+	gc.disable()
 	def cosine_calculation(V_cut, W_cut):
 		np.warnings.filterwarnings('ignore')
 		similarity = spatial.distance.cosine(V_cut, W_cut)
 		similarity = 1 - similarity
 		return similarity
-
-	H_coslist, W_nonzerolist = [], []
+	
+	H_coslist_k, H_coslist_cos, H_coslist_t = [], [], []
 	H_matrow, H_matcol = H_mat.shape[0], H_mat.shape[1]
-	W_mat = W_mat.tocsc()
-	V_mat = V_mat.tocsr()
 	peaknumber_cut, extend_add = 4, [-2,-1,0.5]
 	for k in np.arange(H_matrow): # use H no noise
-		W_keep = np.sort(np.nonzero(W_mat[:,k])[0][:iso_maxnumber]) # sorted non zero indices
-		W_nonzerolist.append(W_keep)
-		W_keep = W_keep[:peaknumber_cut]
-		V_keep = V_mat[W_keep,:].todense()[:peaknumber_cut]
-		W_cut = W_mat[W_keep,k].todense()[:peaknumber_cut]
-
+		# print k, timeit.default_timer()
+		k_count, v_count = 0, 0
+		W_keep = np.sort(np.nonzero(W_mat[:,k])[0][:peaknumber_cut]) # sorted non zero indices
+		V_keep = V_mat[W_keep,:].todense()#[:peaknumber_cut]
+		W_cut = W_mat[W_keep,k].todense()#[:peaknumber_cut]
 		W_keepextend = W_keep.copy()	
 		for n in extend_add:
 			front_extend = W_keep[0] + int(n*(W_keep[1] - W_keep[0]))
 			if front_extend >= 0: # ignore if negative
-				W_keepextend = np.sort(np.insert(W_keepextend, 0, front_extend))
+				W_keepextend = np.insert(W_keepextend, 0, front_extend)
 				V_keepextend = V_mat[W_keepextend,:].todense()
 				W_cutextend = W_mat[W_keepextend,k].todense()
-
 		t_range = np.nonzero(H_mat[k,:])[1]
 		t_min, t_max = t_range[0], t_range[-1]
 		for t in np.arange(H_matcol):
 			if t_min <= t <= t_max:
 				V_cut = V_keep[:,t]
-				V_cutextend = V_keepextend[:,t]
-				similarity_list = []				
-				similarity_list.append(cosine_calculation(V_cutextend, W_cutextend)) # 
-				similarity_list.append(cosine_calculation(V_cut, W_cut)) # 3 + back
-				arg_back, arg_forth = int(t-1), int(t+1)
-				arg_back = 0 if arg_back < 0 else int(t-1)
-				arg_forth = V_keep.shape[1]-1 if arg_forth > V_keep.shape[1]-1 else int(t+1)
-
-				similarity_list.append(cosine_calculation(V_keep[:,arg_back], W_cut))
-				similarity_list.append(cosine_calculation(V_keep[:,arg_forth], W_cut))
-				similarity_list.append(cosine_calculation(np.sum(V_keep[:,arg_back:arg_forth+1],axis=1), W_cut))
-				similarity_list = np.nan_to_num(similarity_list)
-				H_coslist.append((np.median(similarity_list) + np.mean(similarity_list))/2)
-			else:
-				H_coslist.append(0)
-
-	H_cosmat = np.asarray(H_coslist).reshape((H_matrow, H_matcol))
-	# print ' chk H_mat non zeros', np.count_nonzero(H_mat)
-	# print ' process dub cosine less than or equal:', cos_cutoff
-	H_cosmatdub = H_cosmat.copy()
-	H_cosmatdub[H_cosmatdub<cos_cutoff] = 0
-	H_mat = np.multiply(H_mat, H_cosmatdub)
-	# print ' chk H_cosmat non zeros', np.count_nonzero(H_cosmat), H_cosmat.shape
-	del(H_cosmatdub)
-	return H_mat, sparse.coo_matrix(H_cosmat), W_nonzerolist
+				# pass if V_cut is nothing or has only one peak
+				if np.sum(V_cut) != 0 and np.count_nonzero(V_cut) >= 2 and V_cut[0] != 0:
+					V_cutextend = V_keepextend[:,t]
+					similarity_list = []				
+					similarity_list.append(cosine_calculation(V_cutextend, W_cutextend)) # 
+					similarity_list.append(cosine_calculation(V_cut, W_cut)) # 3 + back
+					arg_back, arg_forth = int(t-1), int(t+1)
+					arg_back = 0 if arg_back < 0 else int(t-1)
+					arg_forth = V_keep.shape[1]-1 if arg_forth > V_keep.shape[1]-1 else int(t+1)
+					similarity_list.append(cosine_calculation(V_keep[:,arg_back], W_cut))
+					similarity_list.append(cosine_calculation(V_keep[:,arg_forth], W_cut))
+					similarity_list.append(cosine_calculation(np.sum(V_keep[:,arg_back:arg_forth+1],axis=1), W_cut))
+					similarity_list = np.nan_to_num(similarity_list)
+					similarity_score = (np.median(similarity_list) + np.mean(similarity_list))/2
+					del(similarity_list)
+					if similarity_score > cos_cutoff:
+						H_coslist_cos.append(similarity_score)
+						H_coslist_t.append(t)
+						k_count += 1
+		H_coslist_k.extend([k]*k_count)
+		# print '*', v_count
+		
+	H_cosmatdub = sparse.csr_matrix((H_coslist_cos, (H_coslist_k, H_coslist_t)), shape=(H_matrow, H_matcol))
+	del(H_coslist_cos)
+	del(H_coslist_t)
+	del(H_coslist_k)
+	try:
+		# H_mat is sparse
+		H_mat = H_cosmatdub.multiply(H_mat)
+	except:
+		# H_mat is dense
+		H_mat = np.multiply(H_mat, H_cosmatdub.todense())
+	return H_mat, H_cosmatdub
 			
